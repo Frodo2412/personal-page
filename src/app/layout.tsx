407e5ebe144cd9c9df4659d62500647ee93d371e
bruno.lemus <brunolemus553@gmail.com>
2023-06-27 18:16:52 -0300

    Moved Contact Bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css';
 import React from "react";
-import Contact from "@/app/Contact";
+import Contact from "@/components/Contact";
 
 export default function RootLayout(
     {children}: { children: React.ReactNode }
