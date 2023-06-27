07acb3b7b6e5941d1011dcf085f1545c1a9b0db4
bruno.lemus <brunolemus553@gmail.com>
2023-06-27 18:08:24 -0300

    Remove useless sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Navbar from "@/components/Navbar";
-import Hero from "@/app/Hero";
 import {jobs} from "@/data/Jobs";
 import JobCard from "@/app/JobCard";
 
@@ -18,7 +17,23 @@ export default function Home() {
                         {href: '/#contact', text: "Contact"},
                     ]}
                 />
-                <Hero/>
+                <div className={"w-full min-h-10 flex items-center justify-center p-20 text-white"}>
+                    <div className={"flex bg-slate-900/50 p-10 max-w-6xl md:flex-row flex-col"}>
+                        <p className={"hidden md:block text-6xl text-right max-w-5xl animate-fadeRight"}>
+                            Do you want to build <strong>efficient</strong> software that <strong>scales</strong>?
+                        </p>
+                        <div className={"flex-grow-0 border-gray-300 border-r-2 pr-5 animate-expand"}/>
+                        <div className={"flex flex-col content-center justify-center ml-5 animate-fadeDown"}>
+                            <h1 className={"text-6xl"}>Hello there, I&apos;m Bruno.</h1>
+                            <p className={"text-lg"}>
+                                I&apos;m a software engineer with a passion for big scale, highly concurrent distributed
+                                systems.
+                                I strongly believe the best way to build them is by leveraging the power of functional
+                                programming.
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
 
             <div id={"experience"} className={"flex flex-col justify-around content-around text-lg w-full"}>
